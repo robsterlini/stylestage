20409e278c7226b5567fd10ca4807e13ec62d2d0
Rob Sterlini <7571609+robsterlini@users.noreply.github.com>
2020-10-01 11:52:23 +0100

    Setup gulp boilerplate (#1)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# stylestage
+# Stylestage Gulp
+
+> Minimial boilerplate using Gulp to process style.scss into style.css for Stylestage submissions.
+
+## Installation
+
+```
+npm i
+```
+
+## Development
+
+Spins up a BrowserSync webserver that automatically updates in your browser when changes to style.scss are made. It doesn’t watch anything else as the scope of this project is limited to one stylesheet.
+
+```
+gulp serve
+```
+
+## Build
+
+```
+gulp build
+```
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('scss/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build'))
+    .pipe(reload({
+      stream: true,
+    }));
+});
+
+gulp.task('serve', gulp.series('sass', function() {
+  browserSync({
+    server: {
+      baseDir: 'build',
+    },
+  });
+
+  gulp.watch('scss/*.scss', gulp.series('sass'));
+}));
+
+gulp.task('build', gulp.series('sass'));
